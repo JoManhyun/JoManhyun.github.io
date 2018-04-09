dd538c780cf8fc250d5821d6692aecda801542d8
JoManhyun <37892571+JoManhyun@users.noreply.github.com>
2018-04-09 15:59:16 +0900

    바이너리탐색 추가

--- hashDB.sol
+++ hashDB.sol
@@ -3,6 +3,11 @@ pragma solidity ^0.4.21;
 contract HashDB{
 
 	string[] hashArr;
+	struct hashStruct {
+		uint num;
+		string hashValue;
+	}
+	hashStruct[] searchData;
 
 	uint count = 0;
 
@@ -12,8 +17,29 @@ contract HashDB{
 		getArrNum();
 	}
 
-	function getArrNum() public constant returns (uint) {
-	    return count - 1;
+	function binarySearch(uint a)public constant returns(uint, string){
+		uint low = 0;
+		uint mid;
+		uint high = count - 1;
+		uint size = count - 1;
+		hashStruct.num = size;
+		hashStruct.hashValue = hashArr[size];
+		searchData.push(hashStruct);
+
+		while(low <= high) {
+			mid = (low + high)/2;
+			if(a == searchData[size].num && hashArr[a] == searchData[size].hashValue) {
+				return mid;
+			}
+			else if(a > searchData[size].num) {
+				low = mid + 1;
+			}
+			else {
+				high = mid - 1;
+			}
+		}
+		return searchData[a].num;
+		return searchData[a].hashValue;
 	}
 
 	function getData(uint a) public constant returns(string) {
