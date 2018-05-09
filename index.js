e39d793f5ce771d91154912fcda5d059aa5667b9
JoManhyun <37892571+JoManhyun@users.noreply.github.com>
2018-05-09 19:04:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 (function() {
     Web3 = require('web3');
-    web3 = new Web3(new Web3.providers.HttpProvider("http://203.246.30.80:8545"));
+    web3 = new Web3(new Web3.providers.HttpProvider("http://203.246.30.130:8545"));
 })();
